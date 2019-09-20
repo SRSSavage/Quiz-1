614472f6d37fe636ddc05310825af43714f20f79
Zach Hewitt <zach@zachs-mbp.home>
2019-09-20 16:43:21 -0600

    added model/set-up index page

---
+++ app/models/randomchamp.rb
@@ -0,0 +1,2 @@
+class Randomchamp < ApplicationRecord
+end
---
+++ test/models/randomchamp_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RandomchampTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
