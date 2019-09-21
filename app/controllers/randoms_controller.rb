20697a078401e83ffd039ceee0251ddad99abf0a
Zach Hewitt <zach@zachs-mbp.home>
2019-09-20 18:05:16 -0600

    added routes/styling/about/index

--- app/controllers/randoms_controller.rb
+++ app/controllers/randoms_controller.rb
@@ -1,6 +1,9 @@
 class RandomsController < ApplicationController
 
   def index
-    
+    @random = Randomchamp.order("RANDOM()").first
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'randoms#index'
   resources :randoms
+  get 'about', to: 'randoms#about'
 end
